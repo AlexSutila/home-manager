c672db9664ab8e56ac1f423cc4ce3c28655c2a38
AlexSutila <alexsutila2020@gmail.com>
2024-12-13 19:08:24 -0700

    more random lame scripts

---
+++ scripts/swap_caps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For keyboards which I cannot reprogram the actual
+# firmware on. I cannot live with the traditional
+# location of escape - terrible.
+
+# setxkbmap -option "caps:swapescape"
+setxkbmap -option
