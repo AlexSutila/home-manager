0526fcd2d187777d7e12e129a5d25be551b60493
AlexSutila <alexsutila2020@gmail.com>
2025-02-13 17:45:46 -0700

    Make this suck less

--- firefox.nix
+++ firefox.nix
@@ -17,31 +17,32 @@ in
 
     policies = {
 
-        ExtensionSettings = {
-            "*".installation_mode = "blocked";
-
-            "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = { # vimium
-                install_url = "https://addons.mozilla.org/firefox/downloads/file/4259790/vimium_ff-2.1.2.xpi";
-                installation_mode = "force_installed";
-            };
-
-            "{608b1b71-54d9-4606-87b6-ab2c8b48e708}" = { # one dark
-                install_url = "https://addons.mozilla.org/firefox/downloads/file/3775372/one_dark-1.0.xpi";
-                installation_mode = "force_installed";
-            };
-
-            "firefox-compact-dark@mozilla.org" = {
-                install_url = "";
-                installation_mode = "force_installed";
-            };
+        ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
         };
 
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
+        in listToAttrs [
+          (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
+          (extension "one-dark" "{608b1b71-54d9-4606-87b6-ab2c8b48e708}")
+        ];
+
+      # See ~/.mozilla/firefox/<profile-id>/prefs.js
       Preferences = {
         "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
         "extensions.pocket.enabled" = lock-false;
         "extensions.screenshots.disabled" = lock-true;
+        "layout.css.prefers-color-scheme.content-override" = 0; # Puts the thing in dark mode
       };
-
     };
   };
 }
