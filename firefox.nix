b42b3e7aec200108e50649eaaf071ff2aa91e596
AlexSutila <alexsutila2020@gmail.com>
2025-02-20 15:43:55 -0700

    Adapt browser theme to nixcolors

--- firefox.nix
+++ firefox.nix
@@ -1,48 +1,101 @@
-{ ... }:
+{ pkgs, config, inputs, ... }:
 
-let
-  lock-false = {
-    Value = false;
-    Status = "locked";
-  };
-  lock-true = {
-    Value = true;
-    Status = "locked";
-  };
-in
 {
   programs.firefox = {
     enable = true;
     languagePacks = [ "en-US" ];
+    profiles.dorce = {
 
-    policies = {
+      # Plugins
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        vimium
+      ];
 
-        ExtensionSettings = with builtins;
-        let extension = shortId: uuid: {
-          name = uuid;
-          value = {
-            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
-            installation_mode = "normal_installed";
-          };
-        };
-
-        # To add additional extensions, find it on addons.mozilla.org, find
-        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
-        # Then, download the XPI by filling it in to the install_url template, unzip it,
-        # run `jq .browser_specific_settings.gecko.id manifest.json` or
-        # `jq .applications.gecko.id manifest.json` to get the UUID
-        in listToAttrs [
-          (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
-          (extension "one-dark" "{608b1b71-54d9-4606-87b6-ab2c8b48e708}")
-        ];
-
-      # See ~/.mozilla/firefox/<profile-id>/prefs.js
-      Preferences = {
-        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-        "extensions.pocket.enabled" = lock-false;
-        "extensions.screenshots.disabled" = lock-true;
-        "layout.css.prefers-color-scheme.content-override" = 0; # Puts the thing in dark mode
+      # Settings
+      settings = {
+        "extensions.pocket.enabled" = false;
+        "extensions.screenshots.disabled" = true;
+        "layout.css.prefers-color-scheme.content-override" = 0;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.toolbars.bookmarks.visibility" = false;
+        "browser.ui.showBookmarkedTabs" = false;
+        "ui.systemUsesDarkTheme" = 1;
+        "browser.startup.homepage" = "https://www.github.com/AlexSutila";
       };
+
+      userChrome = ''
+        :root {
+          --focus-outline-color: #${config.colorScheme.colors.base04} !important;
+          --toolbar-color: #${config.colorScheme.colors.base07} !important;
+          --tab-min-height: 30px !important;
+        }
+        /* Background of tab bar */
+        .toolbar-items {
+          background-color: #${config.colorScheme.colors.base00} !important;
+        }
+        /* Extra tab bar sides on macOS */
+        .titlebar-spacer {
+          background-color: #${config.colorScheme.colors.base00} !important;
+        }
+        .titlebar-buttonbox-container {
+          background-color: #${config.colorScheme.colors.base00} !important;
+        }
+        #tabbrowser-tabs {
+          border-inline-start: 0 !important;
+        }
+        /* Private Browsing indicator on macOS */
+        #private-browsing-indicator-with-label {
+          background-color: #${config.colorScheme.colors.base00} !important;
+          margin-inline: 0 !important;
+          padding-inline: 7px;
+        }
+        /* Tabs themselves */
+        .tabbrowser-tab .tab-stack {
+          border-radius: 5px 5px 0 0;
+          overflow: hidden;
+          background-color: #${config.colorScheme.colors.base00};
+          color: #${config.colorScheme.colors.base06} !important;
+        }
+        .tab-content {
+          border-bottom: 2px solid color-mix(in srgb, var(--identity-tab-color) 40%, transparent);
+          border-radius: 5px 5px 0 0;
+          background-color: #${config.colorScheme.colors.base00};
+          color: #${config.colorScheme.colors.base06} !important;
+        }
+        .tab-content[selected] {
+          border-bottom: 2px solid color-mix(in srgb, var(--identity-tab-color) 25%, transparent);
+          background-color: #${config.colorScheme.colors.base01} !important;
+          color: #${config.colorScheme.colors.base07} !important;
+        }
+        /* Below tab bar */
+        #nav-bar {
+          background: #${config.colorScheme.colors.base01} !important;
+        }
+        /* URL bar in nav bar */
+        #urlbar[focused=true] {
+          color: #${config.colorScheme.colors.base07} !important;
+          background: #${config.colorScheme.colors.base02} !important;
+          caret-color: #${config.colorScheme.colors.base05} !important;
+        }
+        #urlbar:not([focused=true]) {
+          color: #${config.colorScheme.colors.base04} !important;
+          background: #${config.colorScheme.colors.base02} !important;
+        }
+        #urlbar ::-moz-selection {
+          color: #${config.colorScheme.colors.base07} !important;
+          background: #${config.colorScheme.colors.base02} !important;
+        }
+        #urlbar-input-container {
+          border: 1px solid #${config.colorScheme.colors.base01} !important;
+        }
+        #urlbar-background {
+          background: #${config.colorScheme.colors.base01} !important;
+        }
+        /* Text in URL bar */
+        #urlbar-input, #urlbar-scheme, .searchbar-textbox {
+          color: #${config.colorScheme.colors.base07} !important;
+        }
+      '';
     };
   };
 }
